a2900d1bae976336bc0d5fbc2e1ad55338163202
J4c0bV <oquevoceusquiser@gmail.com>
2024-05-21 10:13:55 -0300

    Inicio da programacao dos controllers

--- api/app/controllers/clinicas_controller.ts
+++ api/app/controllers/clinicas_controller.ts
@@ -1,3 +1,50 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Clinica from '#models/clinica'
+export default class ClinicasController {
+  async index({}: HttpContext) {
+    const clinicas = await Clinica.all()
 
-export default class ClinicasController {}
+    return clinicas
+  }
+
+  async store({ request, response }: HttpContext) {
+    const body = request.body()
+
+    const clinica = await Clinica.create(body)
+
+    response.status(201)
+    return {
+      message: 'Clinica cadastrada com sucesso',
+      clinica,
+    }
+  }
+
+  async show({ params }: HttpContext) {
+    const clinica = await Clinica.findOrFail(params.id)
+
+    return clinica
+  }
+
+  async update({ params, request }: HttpContext) {
+    const body = request.body()
+    const clinica = await Clinica.findOrFail(params.id)
+
+    clinica.id = body.id
+    clinica.nome = body.nome
+    clinica.CNPJ = body.CNPJ
+
+    await clinica.save()
+    return {
+      message: 'Clinica atualizada com sucesso',
+    }
+  }
+
+  async destroy({ params }: HttpContext) {
+    const clinica = await Clinica.findOrFail(params.id)
+    await clinica.delete()
+
+    return {
+      message: 'Clinica deletada com sucesso',
+    }
+  }
+}
--- api/app/controllers/pessoas_controller.ts
+++ api/app/controllers/pessoas_controller.ts
@@ -1,3 +1,56 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Pessoa from '#models/pessoa'
 
-export default class PessoasController {}
+export default class PessoasController {
+  async index({}: HttpContext) {
+    const pessoas = await Pessoa.all()
+
+    return pessoas
+  }
+
+  async store({ request, response }: HttpContext) {
+    const body = request.body()
+
+    const pessoa = await Pessoa.create(body)
+
+    response.status(201)
+    return {
+      message: 'Pessoa cadastrada com sucesso',
+      pessoa,
+    }
+  }
+
+  async show({ params }: HttpContext) {
+    const pessoa = await Pessoa.findOrFail(params.id)
+
+    return pessoa
+  }
+
+  async update({ params, request }: HttpContext) {
+    const body = request.body()
+    const pessoa = await Pessoa.findOrFail(params.id)
+
+    pessoa.id = body.id
+    pessoa.nome = body.nome
+    pessoa.CPF = body.CPF
+    pessoa.RG = body.RG
+    pessoa.email = body.email
+    pessoa.senha = body.senha
+    pessoa.dataDeNascimento = body.dataDeNascimento
+    pessoa.sexo = body.sexo
+
+    await pessoa.save()
+    return {
+      message: 'Pessoa atualizada com sucesso',
+    }
+  }
+
+  async destroy({ params }: HttpContext) {
+    const pessoa = await Pessoa.findOrFail(params.id)
+    await pessoa.delete()
+
+    return {
+      message: 'Pessoa deletada com sucesso',
+    }
+  }
+}
