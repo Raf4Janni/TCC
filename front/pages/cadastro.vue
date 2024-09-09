cb6549dc3e1f04a3c1c7a75f7844c98eca965f9a
J4c0bV <oquevoceusquiser@gmail.com>
2024-09-09 11:06:49 -0300

    alteracoes no cadastro

--- front/pages/cadastro.vue
+++ front/pages/cadastro.vue
@@ -9,37 +9,37 @@
 
                 <label for="">Nome</label>
                 <input type="text"
-                name="" id=""
+                name="" id="nome"
                 placeholder="Digite seu nome"
                 required>
        
                 <label for="">Data de nascimento</label>
                 <input type="date"
-                name="" id=""
+                name="" id="dataNascimento"
                 placeholder="Digite sua data de nascimento"
                 required>
 
                 <label for="">CPF</label>
                 <input type="text"
-                name="" id=""
+                name="" id="cpf"
                 placeholder="Digite seu CPF"
                 required>
 
                 <label for="">E-mail</label>
                 <input type="email"
-                name="" id=""
+                name="" id="email"
                 placeholder="Digite seu e-mail"
                 required>
 
                 <label for="">Senha</label>
                 <input type="password"
-                name="" id=""
+                name="" id="senha"
                 placeholder="Digite sua senha"
                 required>
            
            
             <div class="button-container">
-                <button type="submit" class="button-enviar">Cadastrar</button>
+                <button type="submit" class="button-enviar" @click="carregarDados" >Cadastrar</button>
             </div>
         </form>
         <p class="paragrafo">Já possui uma conta? </p>
@@ -52,4 +52,31 @@
 definePageMeta({
   layout: "nolayout"
 })
+</script>
+<script>
+import {teste} from '../src/Api2';
+
+async function carregarDados() {
+    /*
+    try {
+        const nome = document.getElementById('nome').value;
+        const dataNascimento = document.getElementById('dataNascimento').value;
+        const cpf = document.getElementById('cpf').value;
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+        
+        const data = {
+            nome: nome,
+            dataNascimento: dataNascimento,
+            cpf: cpf,
+            email: email,
+            senha: senha
+        }
+        await teste('pessoas/', data, 'func');
+    } catch (error) {
+        console.error('Erro ao carregar os dados:', error);
+    }
+        */
+}
+
 </script>
