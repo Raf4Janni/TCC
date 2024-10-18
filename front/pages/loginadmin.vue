dde43d78f092c77423903f7a5d8eb9de1f815ac2
VitorHugoGuilhoto <vhs.guilhoto@gmail.com>
2024-10-18 08:29:34 -0300

    Criação da página "loginadmin" e alterações no "style.css"

---
+++ front/pages/loginadmin.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="fade-in">
+      <section class="sectionadm">
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/imgcadastroanimaladm.jfif" /> </div>
+                <h3>Cadastro Animal</h3>
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/funcionario.jpg" /> </div>
+            <h3>Cadastro Funcionario</h3>
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/adocao.jfif" /> </div>
+            <h3>Cadastro Adoções</h3>
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/pessoas.png" /> </div>
+            <h3>Lista Pessoas </h3>
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/listaanimal.jfif" /> </div>
+            <h3>Lista Animais</h3> 
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/historicoadocoes.jfif" /> </div>
+            <h3>Histórico de Adoções</h3>
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+
+        <div class="divadm">
+            <div class="divimgadm"><img class="imgadm" src="/components/assets/img/fichaanamnese.jfif" /> </div>
+            <h3>Ficha Anamnese</h3>
+            <br><button class="btnlink">Ir Para</button>
+        </div>
+       
+      </section>
+    </div>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+ 
+  
